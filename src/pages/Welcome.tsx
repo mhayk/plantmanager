03a89d4abd6bd169e125c2c9ec25a4f6e083c1d7
Mhayk Whandson <hi@mhayk.com>
2021-04-20 11:03:05 +0100

    Welcome page component has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,16 +1,71 @@
 import React from 'react';
-import { Image, Text, View } from 'react-native';
+import { SafeAreaView, Image, Text, Touchable, TouchableOpacity, View, StyleSheet } from 'react-native';
 
 import wateringImg from '../assets/watering.png'
+import colors from '../styles/colors';
 
 export function Welcome() {
     return (
-        <View>
-            <Text>
-                Manage your plants easily
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>
+                Manage {'\n'}
+                your plants {'\n'}
+                easily
             </Text>
 
-            <Image source={wateringImg} />
-        </View>
+            <Image source={wateringImg} style={styles.image} />
+
+            <Text style={styles.subtitle}>
+                Don't forget to water your plants anymore. We take care to
+                remind you whenever you need it.
+            </Text>
+
+            <TouchableOpacity
+                style={styles.button}
+                activeOpacity={0.7}
+            >
+                <Text style={styles.buttonText}>
+                    >
+                </Text>
+            </TouchableOpacity>
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.heading,
+        marginTop: 38
+    },
+    subtitle: {
+        textAlign: 'center',
+        fontSize: 18,
+        paddingHorizontal: 20,
+        color: colors.heading
+    },
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        width: 56
+    },
+    image: {
+        width: 292,
+        height: 284
+    },
+    buttonText: {
+        color: colors.white,
+        fontSize: 24
+    }
+})
